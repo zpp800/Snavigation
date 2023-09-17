379492277f5110ad9be7159eb1b5e4327e4a9ca6
zpp <318745470@qq.com>
2023-09-17 16:23:04 +0800

    Update WeatherTime.vue

--- src/components/WeatherTime.vue
+++ src/components/WeatherTime.vue
@@ -102,7 +102,7 @@ const getWeatherData = () => {
         weatherCity.value = res.result.city;
         lastWeatherData = {
           data: res.result.condition,
-          data: res.result.city,
+          data1: res.result.city,
           lastFetchTime: currentTime,
         };
         // 将新的天气数据和时间戳存储到 localStorage 中
